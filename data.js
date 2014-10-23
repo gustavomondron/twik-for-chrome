9b69b3ce5dc8341728631465b0cd18a689382cee
Gustavo G. Pascual <gustavo.mondron@gmail.com>
2014-10-24 00:04:23 +0200

    Fix: database is not scalable

--- data.js
+++ data.js
@@ -18,7 +18,9 @@
  */
 
 var DB_VERSION = 2;
-
+var PATTERN_SITE_PROFILE = /^site_profile_(.+)$/;
+var PATTERN_SITE_SETTINGS = /^site_settings_([0-9]?)_(.+)$/
+var PATTERN_TAG_SETTINGS = /^tag_settings_([0-9]?)_(.+)$/
 /*
   Provide mechanisms to read and modify all the data used in Twik
 */
@@ -26,7 +28,7 @@ function ProfileList() {
   this.list = {}; // List of profiles
   this.siteProfiles = {}; // Default profile for each website
   this.syncPrivateKeys; // Sync/local storage for private keys
-  this.dbVersion = 1; // Version of database
+  this.dbVersion = 1; // Database version (1 unless overwritten)
 }
 
 /*
@@ -36,7 +38,6 @@ ProfileList.prototype.getFromStorage = function(callback) {
   var ref = this;
   chrome.storage.sync.get(null,
     function(items) {
-      var siteProfiles = items.site_profiles;
       ref.syncPrivateKeys = items.sync_private_keys;
       var profiles = items.profiles;
 
@@ -44,8 +45,49 @@ ProfileList.prototype.getFromStorage = function(callback) {
         ref.dbVersion = items.db_version;
       }
 
-      if (siteProfiles != null) {
-        ref.siteProfiles = siteProfiles;
+      // Get sites' tag and enabled inputs for each profile
+      if (ref.dbVersion >= 2) {
+        // Sites settings are stored in distinct objects
+        // Initialize sites lists
+        for(profileKey in profiles) {
+          profiles[profileKey].sites = {};
+        }
+        Object.keys(items).forEach(function(key) {
+          if (key.indexOf("site_settings_") == 0) {
+            var keyParts = PATTERN_SITE_SETTINGS.exec(key);
+            var profileKey = keyParts[1];
+            var site = keyParts[2];
+            profiles[profileKey].sites[site] = items[key];
+          }
+        });
+      }
+
+      // Get tag specific settings for each profile
+      if (ref.dbVersion >= 2) {
+        for (profileKey in profiles) {
+          profiles[profileKey].tags = {};
+        }
+        Object.keys(items).forEach(function(key) {
+          if (key.indexOf("tag_settings_") == 0) {
+            var keyParts = PATTERN_TAG_SETTINGS.exec(key);
+            var profileKey = keyParts[1];
+            var tag = keyParts [2];
+            profiles[profileKey].tags[tag] = items[key];
+          }
+        });
+      }
+
+      // Get default profile for each site
+      if (ref.dbVersion < 2 && items.site_profiles != null) {
+        ref.siteProfiles = items.site_profiles;
+      } else if (ref.dbVersion >= 2) {
+        ref.siteProfiles = {};
+        Object.keys(items).forEach(function(key) {
+          if (key.indexOf("site_profile_") == 0) {
+            var site = PATTERN_SITE_PROFILE.exec(key)[1];
+            ref.siteProfiles[site] = items[key];
+          }
+        });
       }
 
       // Get private keys from local storage if they are not synced
@@ -56,28 +98,50 @@ ProfileList.prototype.getFromStorage = function(callback) {
             profileCount = keys.length;
             for (i = 0; i < profileCount; i++) {
               // Be careful: database v2 introduces a change in private keys indexes for local storage
-              if (this.dbVersion < 2) {
+              if (ref.dbVersion < 2) {
                 profiles[keys[i]].private_key = localItems.private_keys[i];
               } else {
                 profiles[keys[i]].private_key = localItems.private_keys[keys[i]];
               }
             }
           }
+
           // Clone profiles object
           ref.list = jQuery.extend({}, profiles);
-          if (callback != null) {
-            callback();
-          }
+          
+          // Update database if necessary and execute callback
+          ref.updateDatabase(callback);
         });
       } else {
         // Clone profiles object
         ref.list = jQuery.extend({}, profiles);
-        if (callback != null) {
-          callback();
-        }
+
+        // Update database if necessary and execute callback
+        ref.updateDatabase(callback);
       }
     }
   );
+}
+
+/*
+  Force a database version if it is outdated
+  Args: 
+    callback: function called when the process is completed
+*/
+ProfileList.prototype.updateDatabase = function(callback) {
+  if (this.dbVersion < DB_VERSION) {
+    if (this.dbVersion == 1) {
+      // Remove legacy objects
+      var ref = this;
+      chrome.storage.sync.remove("site_profiles", function() {
+        ref.setToStorage(callback);
+      });
+    } else {
+      this.setToStorage(callback);
+    }
+  } else if (callback != null) {
+    callback();
+  }
 }
 
 /*
@@ -114,13 +178,27 @@ ProfileList.prototype.setProfile = function(key, profile) {
 }
 
 /*
-  Remove a profile
+  Remove a profile.
+  WARNING: This change is automatically updated in storage.
   Args:
     key: the profile key
 */
-ProfileList.prototype.removeProfile = function(key) {
+ProfileList.prototype.removeProfile = function(key, callback) {
+  var ref = this;
   if ($.inArray(key, this.getKeys()) != -1) {
-    delete this.list[key];
+    var objectsToRemove = [];
+    // Remove site-specific settings for this profile
+    for (site in this.list[key].sites) {
+      objectsToRemove.push("site_settings_" + key + "_" + site);
+    }
+    // Remove tag-specific settings for this profile
+    for (tag in this.list[key].tags) {
+      objectsToRemove.push("tag_settings_" + key + "_" + tag);
+    }
+    chrome.storage.sync.remove(objectsToRemove, function() {
+      delete ref.list[key];
+      ref.setToStorage(callback);
+    });
   }
 }
 
@@ -149,6 +227,63 @@ ProfileList.prototype.getPrivateKeys = function() {
 }
 
 /*
+  Update site settings
+  Args:
+    profileKey: profile key
+    site: site domain
+    tag: tag name
+    enabled_inputs: list of password inputs managed by Twik
+*/
+ProfileList.prototype.setSiteSettings = function(profileKey, site, tag, enabled_inputs) {
+  if (this.list[profileKey].sites[site] != null) {
+    // Check if tag has changed
+    var oldTag = this.list[profileKey].sites[site].tag;
+    if (oldTag != tag) {
+      // Remove old tag if it is no longer used
+      var used = false;
+      for (s in this.list[profileKey].sites) {
+        used = s != site && oldTag == this.list[profileKey].sites[s].tag;
+        if (used) {
+          break;
+        }
+      }
+
+      // Add new tag
+      this.list[profileKey].tags[tag] = {
+        password_length: oldTag.password_length,
+        password_type: oldTag.password_type
+      };
+        
+      // Delete old tag if it is no longer used
+      if (!used) {
+        delete this.list[profileKey].tags[oldTag];
+        chrome.storage.sync.remove("tag_settings_" + profileKey + "_" + oldTag, null);
+      }
+    }
+  }
+
+  this.list[profileKey].sites[site] = {
+    tag: tag,
+    enabled_inputs: enabled_inputs
+  };
+}
+
+/*
+  Update tag settings
+  Args:
+    profileKey: profile key
+    tag: tag name
+    passwordLength: length of password
+    passwordType: type of password
+*/
+ProfileList.prototype.setTagSettings = function(profileKey, tag, passwordLength, passwordType) {
+  this.list[profileKey].tags[tag] = {
+    password_length: passwordLength,
+    password_type: passwordType
+  };
+}
+
+/*
   Saves the data to storage
   Args:
     callback: function called when the process is completed
@@ -156,23 +291,48 @@ ProfileList.prototype.getPrivateKeys = function() {
 ProfileList.prototype.setToStorage = function(callback) {
   var ref = this;
   var profilesToStorage;
-  if (this.syncPrivateKeys) {
-    profilesToStorage = this.list;
-  } else {
-    keys = this.getKeys();
-    // Clone object
-    profilesToStorage = JSON.parse(JSON.stringify(this.list));
-    for (var i = 0; i < keys.length; i++) {
-      profilesToStorage[keys[i]].private_key = '';
+  
+  // Remove private keys from profiles sync if necessary
+  // Clone object
+  profilesToStorage = JSON.parse(JSON.stringify(this.list));
+  if (!this.syncPrivateKeys) {
+    for (profileKey in this.list) {
+      profilesToStorage[profileKey].private_key = '';
     }
   }
 
-  chrome.storage.sync.set({
+  // Initialize objects to sync
+  var objectsToSync = {
     db_version: DB_VERSION,
-    profiles: profilesToStorage,
     sync_private_keys: ref.syncPrivateKeys,
-    site_profiles: ref.siteProfiles
-  }, function() {
+  };
+
+  // Separate site-specific tag and enabled inputs in distinct objects
+  for (profileKey in profilesToStorage) {
+    var sites = profilesToStorage[profileKey].sites;
+    for (site in sites) {
+      objectsToSync["site_settings_" + profileKey + "_" + site] = sites[site];
+    }
+    delete profilesToStorage[profileKey].sites;
+  }
+
+  // Separate tag-specific settings in distinct objets
+  for (profileKey in profilesToStorage) {
+    var tags = profilesToStorage[profileKey].tags;
+    for (tag in tags) {
+      objectsToSync["tag_settings_" + profileKey + "_" + tag] = tags[tag];
+    }
+    delete profilesToStorage[profileKey].tags;
+  }
+
+  // Separate default profile for each site in distinct objects
+  for (site in ref.siteProfiles) {
+    objectsToSync["site_profile_" + site] = ref.siteProfiles[site];
+  }
+
+  objectsToSync['profiles'] = profilesToStorage;
+
+  chrome.storage.sync.set(objectsToSync, function() {
     chrome.storage.local.set({
       private_keys: ref.getPrivateKeys()
     }, function() {
@@ -184,7 +344,7 @@ ProfileList.prototype.setToStorage = function(callback) {
 }
 
 /*
-  Create the default profile and saves it to storage
+  Create the default profile and save it to storage
   Args:
     callback: function called when the process is completed
 */
@@ -194,19 +354,22 @@ ProfileList.prototype.createDefaultProfile = function(callback) {
     private_key: generatePrivateKey(),
     password_type: PASSWORD_TYPES.ALPHANUMERIC_AND_SPECIAL_CHARS,
     password_length: 12,
-    color: 1,
-    tags: {},
-    sites: {}
+    color: 1
   };
-  
-  this.list[this.getNewKey()] = profile;
+
+  // Add attributes for this profile list (not for storage)  
+  var thisProfile = JSON.parse(JSON.stringify(profile));
+  thisProfile.sites = {};
+  thisProfile.tags = {};
+  this.list[this.getNewKey()] = thisProfile;
+
+  // Default: do not sync private keys
   this.syncPrivateKeys = false;
-  var ref = this;
-  
+
+  var ref = this;  
   chrome.storage.sync.set({
      profiles: [ profile ],
-     sync_private_keys: false,
-     site_profiles: {}
+     sync_private_keys: false
     }, function() {
       chrome.storage.local.set(
         { private_keys: ref.getPrivateKeys() },
@@ -231,8 +394,10 @@ ProfileList.prototype.setProfileForSite = function(site, key, callback) {
   var ref = this;
   if ($.inArray(key, this.getKeys()) != -1) {
     this.siteProfiles[site] = key;
+    var objectToSync = {};
+    objectToSync["site_profile_" + site] = key;
     chrome.storage.sync.set(
-      { site_profiles: ref.siteProfiles },
+      objectToSync,
       function() {
         if (callback != null) {
           callback();
@@ -271,4 +436,3 @@ ProfileList.prototype.getSyncPrivateKeys = function() {
 }
 
 // ---------------- END OF ProfileList ----------------- //
-
